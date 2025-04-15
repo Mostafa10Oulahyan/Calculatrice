227824104eb1fc850515065be9bc03a8e52649a2
Mostafa Oulahyan <mostafaoulahyan@gmail.com>
2025-04-15 19:04:18 +0100

    Update Errors

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -25,8 +25,8 @@ let cos=document.getElementById("cos")
 let sin=document.getElementById("sin")
 let tan=document.getElementById("tan")
 let sqrt=document.getElementById("sqrt")
-let Math=[cos,sin,tan,sqrt]
-Math.forEach(elt=>{
+let a=[cos,sin,tan,sqrt]
+a.forEach(elt=>{
     elt.style.color="yellow"
 })
 let point=document.getElementById("point")
@@ -35,20 +35,49 @@ let C=document.getElementById("C").style.color="red"
 let num=""
 let operation = document.getElementById("operation");
 let res = document.getElementById("res");
+
+let history = document.getElementById("history")
+function addToHistory(op, result) {
+    let item = document.createElement("div")
+    item.innerHTML = `${op} = <strong>${result}</strong>`
+    history.prepend(item) 
+}
+$('#H').on("click", function () {
+    $('#history').slideToggle(400)
+})
+function UpdateClear(){
+    if(document.querySelectorAll("#history > div").length==0){
+        document.getElementById("btn").style.display="none"
+    }
+    else{
+        document.getElementById("btn").style.display="block"
+    }
+}
+document.getElementById("btn").onclick=function(){
+    let historyDivs = document.querySelectorAll("#history > div");
+    historyDivs.forEach(d => d.remove())
+    operation.innerHTML = ""
+    res.innerHTML = ""
+    UpdateClear()
+}
+
 document.querySelectorAll("input[type=button]").forEach(buttons => {
     buttons.addEventListener("click", function () {
         let value = this.value
-        if (value == "C") {
+        if (operation.innerHTML==""){
+            res.innerHTML=""    
+        }
+        if (value === "C") {
             num = ""
             operation.innerHTML = ""
             res.innerHTML = ""
         }
-        else if (value == "CE") {
+        else if (value === "CE") {
             num = num.slice(0, -1)
             operation.innerHTML = num
             res.innerHTML = ""
         }
-         else if (value == "=") {
+         else if (value === "=") {
             try {
                 num = eval(num.replace("sqrt", "Math.sqrt")
                     .replace("cos", "Math.cos")
@@ -56,15 +85,28 @@ document.querySelectorAll("input[type=button]").forEach(buttons => {
                     .replace("tan", "Math.tan")
                     .replace("%","*0.01"))
                 res.innerHTML = num
+                if (operation.innerHTML && res.innerHTML && res.innerHTML !== "Error") {
+                    addToHistory(operation.innerHTML, res.innerHTML);
+                    UpdateClear()
+                }
             } catch {
                 num = "Error"
                 res.innerHTML = num
             }
         }
-        else {
+        else if(value!=="🕘" ) {
             num += value
             operation.innerHTML = num
         }
     })
 })
+//    JS  TOogle HHHHHh
+const TOGGLE_BTN = document.getElementById("toggleBtn");
 
+const TOGGLE = () => {
+    const IS_PRESSED = TOGGLE_BTN.matches("[aria-pressed=true]");
+    document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
+    TOGGLE_BTN.setAttribute("aria-pressed", IS_PRESSED ? false : true);
+};
+
+TOGGLE_BTN.addEventListener("click", TOGGLE);
